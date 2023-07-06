849c9bae249abc83d35fec28db477cae485cf22b
dgmolokov <dg.molokov@gmail.com>
2023-07-06 12:08:16 +0700

    removed "ResponseEntity<>" return type from ProductsController methods

--- src/main/java/com/example/cart_command_line_app/controller/ProductsController.java
+++ src/main/java/com/example/cart_command_line_app/controller/ProductsController.java
@@ -3,8 +3,6 @@ package com.example.cart_command_line_app.controller;
 import com.example.cart_command_line_app.jpa.Product;
 import com.example.cart_command_line_app.service.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +14,18 @@ public class ProductsController {
   private final ProductService productService;
 
   @GetMapping("/all")
-  public ResponseEntity<List<Product>> getAllProducts() {
-    return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
+  public List<Product> getAllProducts() {
+    return productService.findAll();
   }
 
   @GetMapping
-  public ResponseEntity<Product> getProduct(@RequestParam long id) {
-    return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
+  public Product getProduct(@RequestParam long id) {
+    return productService.getProductById(id);
   }
 
   @PostMapping
-  public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-    return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
+  public Product createProduct(@RequestBody Product product) {
+    return productService.createProduct(product);
   }
 
   @DeleteMapping
